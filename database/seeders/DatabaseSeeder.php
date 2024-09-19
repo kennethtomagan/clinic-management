65cad74fe57222c8d3e64f3057bdd304490ea6e6
Kenneth Tomagan <kenneth.tomagan123@gmail.com>
2024-09-19 10:00:11 +0800

    Updated the seeder file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,10 +17,12 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+        User::create([
             'name' => 'Admin User',
             'email' => 'admin@admin.com',
-            'password' => 'Admin123',
+            'email_verified_at' => now(),
+            'password' => Hash::make('Admin123'),
+            'remember_token' => Str::random(10),
         ]);
     }
 }
